f9b14e560fa6715b1d2b0f5ebc4ef9b806f66736
Christiano0407 <placebo0704@gmail.com>
2023-02-13 11:41:33 -0600

    new application and express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 //** === ==== Application Books ===== ===  */
+const express = require(`express`);
+const app = express();
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running in port: http://localhost:${port}}`);
+});
