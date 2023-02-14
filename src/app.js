404b81bc362f0a2da66af7c6d19dbb04d859df3d
Christiano0407 <placebo0704@gmail.com>
2023-02-14 11:54:13 -0600

    error 404 not found

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//** ==== RUN Application ==== === APP === */
+//** ==== ====================== ===  RUN Application === ================================  === APP === */
 const express = require(`express`);
 const path = require(`path`); // == Concat ===
 const app = express();
@@ -10,15 +10,20 @@ app.set(`views`, path.join(__dirname, `views`));
 //**! === Add Ejs === */
 app.set(`view engine`, `ejs`);
 
-//**? === Add Middleware ===  */
+//**! === Add Middleware ===  */
 app.use(morgan(`dev`));
 app.use(express.urlencoded({ extended: false })); // => Formulary
 
 //**! === Router ===  */
 app.use(require(`./router/index.js`));
 
-//**? === Static === HTML */
+//**! === Static === HTML */
 app.use(express.static(path.join(__dirname, `public`))); // ==> Public
+
+//**! === 404 Handler === Status */
+app.use((req, res, next) => {
+  res.status(404).send(`404 Not Found`);
+});
 
 //** === Module Exportation  === */
 module.exports = app;
