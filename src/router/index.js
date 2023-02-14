6d43c369b423d68189d7a34a2bb1ed8ec37911fe
Christiano0407 <placebo0704@gmail.com>
2023-02-14 12:14:34 -0600

    new application books partials

--- src/router/index.js
+++ src/router/index.js
@@ -2,4 +2,9 @@
 const { Router } = require(`express`);
 const router = Router();
 
+//** === Init */
+router.get(`/`, (req, res) => {
+  res.send(`Hello Home, World`);
+});
+//** === Module Export */
 module.exports = router;
