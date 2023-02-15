48640b7e1d62a07ca258de734c8562bcb16fb047
Christiano0407 <placebo0704@gmail.com>
2023-02-15 11:46:42 -0600

    new container && pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,19 @@
 const { Router } = require(`express`);
 const router = Router();
 
-//** === Init */
+//** === Init === Pagination === */
 router.get(`/`, (req, res) => {
   //res.send(`Hello Home, World`);
   res.render(`../views/index.ejs`);
 });
+
+router.get(`/new-entry`, (req, res) => {
+  res.render(`../views/newEntry.ejs`);
+});
+
+router.get(`/page-404`, (req, res) => {
+  res.render(`../views/404.ejs`);
+});
+
 //** === Module Export */
 module.exports = router;
